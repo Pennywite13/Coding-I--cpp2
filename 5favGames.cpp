4affd6970a6e80f17dda2b6e0c4f9caefb87fd5b
Pennywite13 <skmansard@gmail.com>
2025-02-25 13:26:59 -0600

    finished homework
    Fixed homework to function fully correctly

--- 5favGames.cpp
+++ 5favGames.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 
 int main() {
+    string games[5]; //there can be five games, with values from 0-4s
+    int currentElement = 0;
     while (true){
 
         //preping options and showing player
@@ -16,44 +18,42 @@ int main() {
         cout << "Press 4 to view your list of games.\n";
         cout << "Press 5 to quit the program.\n";
 
-        string input1;			// defining input from user
-        getline(cin, input1);	// get the input from the player
-        int currentElement = 0;
+        string input;			// defining input from user
+        getline(cin, input);	// get the input from the player
 
         //making array
-        string games[5]; //there can be five games, with values from 0-4s
 
         int n = sizeof(games) / sizeof(games[0]); //for remove, copy paste from online
 
-        if (stoi(input1) == 1) { //COMPLETE (not sure if actually getting the input)
+        if (stoi(input) == 1) { //COMPLETE 
             cout << "Let's add a game to your list.\n";
             cout << "Please type the name of the game you want to add.\n";
             string addinput;
             getline (cin, addinput);
-            do {
-                games[currentElement++] = addinput;
-            } while (currentElement < 5);
+            games[currentElement++] = addinput;
+
         }
-        else if (stoi(input1) == 2) { //COMPLETE
+        else if (stoi(input) == 2) { //COMPLETE
             cout << "Let's edit a games name.\n";
             cout << "What is the current name in our system? \n";
             string wrongName ;
             getline(cin, wrongName);
 
             for(int i = 0; i < 5; i++){ //looking through array
+                int nameCount = 5;
                 if(games[i] == wrongName){
                     cout << "We have found that game in our system. What would you like to change it to? \n";
                         
                     string fixedName;
                     getline(cin, fixedName); //input for what it should now be called
-                    swap (wrongName, fixedName); //puts fixed name into array
+                    games[i] = fixedName;
                 }
-                else { //result if input isnt in the system
+                if(i+1 == nameCount){ //result if input isnt in the system
                     cout << "Sorry, the game you entered doesn't seem to be on your list.\n";
                 }
             }
         }
-        else if (stoi(input1) == 3) { //slightly copy paste. Complete???
+        else if (stoi(input) == 3) { //COMPLETE
             cout << "Let's remove a game.\n";
             cout << "What game would you like to remove? \n";
             string gameRemove;
@@ -72,7 +72,7 @@ int main() {
                 }
             }
         }
-        else if (stoi(input1) == 4) { //COMPLETE (isnt actually showing games)
+        else if (stoi(input) == 4) { //COMPLETE 
             cout << "Let's view your current list of games.\n";
             cout << "Your current list of favorite games are: \n";
             for (int i = 0; i < currentElement; i++) {
@@ -80,7 +80,7 @@ int main() {
                 
             }
         }
-        else if (stoi(input1) == 5) { //COMPLETE
+        else if (stoi(input) == 5) { //COMPLETE
             cout << "Quitting program, have a lovely day.\n";
             break;
         }
@@ -92,18 +92,3 @@ int main() {
 
 	return 0;
 }
-
-
-//probally isnt getting the input correctly, find some way to test and fix that before moving on
-
-
-
-//assignment notes from blackboard
-/*
-create a program that will:
-1. Add a game to an array of strings
-2. Edit the name of the game
-3. Remove the game
-4. Show the List
-5. Quit
-*/
