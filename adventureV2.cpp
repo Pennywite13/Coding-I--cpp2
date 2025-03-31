01faf75f9b9a5da45335b860274ca44b9bdc0cd6
Pennywite13 <skmansard@gmail.com>
2025-03-31 17:26:51 -0500

    adventure v2 update
    changes made to get it to a good place, likely last update for mar 31 because I need a brain break

--- adventureV2.cpp
+++ adventureV2.cpp
@@ -7,21 +7,29 @@
 #include <string>       // allows us to use string variables.
 #include <cstdlib>
 #include <ctime>
+using namespace std;
+
+//global variables
+int playerHealth = 10;
+int enemy1Health = 5;
+int turns = 0;
+int totalTreasure = 0;
+
+
 
 //start of functions
 void story(){
     cout << "Welcome hero! Time to start your adventure.\n";
     cout << "While resting at a tavern, you spot a job to fight some goblins. \n";
 
-    cout << "Will you accept your found quest? \n";
-    string givenYesNo;
-    getline(cin,givenYesNo);
 }
 
 string AskYesNo(string givenYesNo){
+
+
     if(givenYesNo == "yes"){
         cout << "Amazing! Best of luck on your adventure. \n";
-        adventure(); //needs user input in parentheses. call later or in main?
+        //adventure(); //needs user input in parentheses. call later or in main?
 
     }
 
@@ -65,13 +73,19 @@ bool adventure(string playerInput){
         //should call rollDie() function
         cout << "The best defence is a good offence. \n";
         cout << "You decide to attack! \n";
-        rollDie();   //call function
         //check if number rolled hits
         //if else statement for if it hits. damage delt included
-        if(diceRoll <= 2){  
+        if(rollDie() <= 2){  
             int playerDamage = (rand()%4+1);
             cout << "Wow! Your attack did " << playerDamage << " points of damage! \n";
             enemy1Health = (enemy1Health-playerDamage) ;
+            if (enemy1Health <= 0){
+                int randomLoot = (rand()%3+1);
+
+                cout << "You did it, you defeated the goblin! \n";
+                cout << "After the battle, you find a stash of " << randomLoot << " hidden away. \n";
+                totalTreasure = totalTreasure + randomLoot;
+            }
         }
         else{
             cout << "Oh no! Your attack missed. \n";
@@ -101,92 +115,38 @@ bool adventure(string playerInput){
 }
 
 
-//global variables
-int playerHealth = 10;
-int enemy1Health = 5;
-int turns = 0;
-int turnsTillEnd = (rand()%6+1); //started writing code for random timer, ran out of time due to other homework
-int totalTreasure = 0;
 
-using namespace std;
 int main() {
-//welcome the player
-    story();
+    srand(time(0)); //seed the random number generator
 
-//    seed the random number generator
-    srand(time(0));
+//welcome the player
+story();
 
 //start the loop
     while(playerHealth > 0) {
+        cout << "Will you accept your found quest? \n";
+        string givenYesNo;
+        getline(cin,givenYesNo);
+        AskYesNo(givenYesNo);
+
         turns +=1;
         cout << "There is a goblin! What would you like to do? \n";
         cout << "press 'a' to attack, press 'b' to block \n";
         string playerInput;
         cin >> playerInput;
-        adventure(playerInput);
+
+        adventure(playerInput); //pulls player input and runs adventure function
+
+        //continue calling functions from here, stopped here on mar 31
 
 
 
-        
-        if (playerInput == "a"){
-            int playerDamage = (rand()%4+1);
-            cout << "Wow! Your attack did " << playerDamage << " points of damage! \n";
-            enemy1Health = (enemy1Health-playerDamage) ;
-
-            if (enemy1Health > 0){
-                int enemy1Damage = (rand()%4+1);
-                cout << "The goblin attacks back doing " << enemy1Damage << " points of damage to you! \n";
-                playerHealth = (playerHealth-enemy1Damage) ;
-            }
-            else{
-                cout << "You did it hero, you won the fight! \n";
-                cout << "Hero, do you wish to keep playing? \n";
-                string input;
-                cin >> input;
-                
-                if (input == "yes"){
-                    cout << "Time to go on another adventure! \n";
-                }
-                else {
-                    cout << "May we see you again another time hero. \n";
-                    break;
-                    }
-                }
-        
-
-        }
-        if (playerInput == "b"){
-            int playerBlock = (rand()%5);
-            int enemyBlock = (rand()%5);
-            if (playerBlock > enemyBlock){
-                cout << "Amazing block! You took no damage. \n";
-            }
-            else {
-                int enemyDamage = (rand()%5);
-                cout << "Oh no! You shield did not block the attack. \n";
-                cout << "You took " << enemyDamage << " points of damage.\n";
-                playerHealth = playerHealth - enemyDamage ;
-            }
-
-        }
-
-    }
-    if(playerHealth <= 0) {
-        cout << "Oh no! The hero has been defeated! \n";
-        while (true){
-            cout << "Hero, do you wish to keep playing? \n";
-            string input;
-            cin >> input;
-        
-            if (input == "yes"){
-                cout << "Time to go on another adventure! \n";
-            }
-            else {
-                cout << "May we see you again another time hero. \n";
-                break;
-            }
-         }
-    }
     
+    }
 }
 
+/*
+int main(){
+    cout << "this is a test. \n";
+}
+*/
