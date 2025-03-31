6dff8dfb7f233d507a3ab41156cf6d8ef43b7185
Pennywite13 <skmansard@gmail.com>
2025-03-31 14:21:47 -0500

    adventure v2

--- adventure v2.cpp
+++ adventure v2.cpp
@@ -9,22 +9,100 @@
 #include <ctime>
 
 //start of functions
+//need story, askyesno, rolldie, ending, adventure
+void story(){
+    //use cout to print story of them getting news of an assignment
+    cout << "Welcome hero! Time to start your adventure.\n";
+    cout << "While resting at a tavern, you spot a job to fight some goblins. \n";
+}
 
+string AskYesNo(string givenYesNo){
+    //cout << "Will you accept your found quest? \n";
+    //string givenYesNo;
+    //getline(cin,givenYesNo)
+    //put above commented out code in main()
+
+    if(givenYesNo == "yes"){
+        cout << "Amazing! Best of luck on your adventure. \n";
+        adventure(); //needs user input in parentheses. call later or in main?
+
+    }
+
+    if(givenYesNo == "no"){
+        cout << "sometimes its best to quit while youre ahead, and prioratize safety. \n";
+        ending();
+    }
+
+    else{
+        cout << "That input is not reconized. \n";
+        cout << "Make sure you use lowercase letters and try again. \n";
+    }
+}
+
+int rollDie(){
+    //make code that rolls a 6 sided die
+    //should be able to copy paste previous code here
+}
+
+void ending(){
+    if (playerHealth <= 0){ //player is dead
+        cout << "You adventure has come to an end, but not by choice. \n";
+        cout << "You die on the battlefield, leaving " << totalTreasure << "behind where you lie.\n";
+    }
+
+    else{  //player is alive
+        cout << "Your adventure has come to an end.\n";
+        cout << "You retire with " << totalTreasure << " treasure to your name.\n";
+        //something to offically end code/break?
+        //(break isnt working because not in a loop)
+    }
+}
+
+bool adventure(string playerInput){
+    if(playerInput == "a"){
+        //code for attack
+        //should call rollDie() function
+        cout << "The best defence is a good offence. \n";
+        cout << "You decide to attack! \n";
+        //call function
+        //check if number rolled hits
+        //if else statement for if it hits. damage delt included
+    }
+
+    if(playerInput =="b"){
+        //code for block
+        //should call rollDie() function
+        cout << "You decide to block an oncoming attack! \n";
+        //call function
+        //check if number rolled blocks
+        //if else statement for if it blocks. Damage taken included
+    }
+
+    else{
+        cout << "That input is not reconized. \n";
+        cout << "Make sure you use lowercase letters and try again. \n";
+    }
+    //code that lets the player take actions
+    //use numbers to let player input decisions
+    //use if statements to determine number input/action wanted
+    //else statement for invalid input
+}
+
+//global variables
+int playerHealth = 10;
+int enemy1Health = 5;
+int turns = 0;
+int turnsTillEnd = (rand()%6+1); //started writing code for random timer, ran out of time due to other homework
+int totalTreasure = 0;
 
 using namespace std;
 int main() {
 //welcome the player
-    cout << "Welcome hero! Time to start your adventure.\n";
-//set up the game
-//    int health = 10, attack, block, turns = 0
-    int playerHealth = 10;
-    int enemy1Health = 5;
-    int turns = 0;
-    int turnsTillEnd = (rand()%6+1); //started writing code for random timer, ran out of time due to other homework
-
+    story();
 
 //    seed the random number generator
     srand(time(0));
+    
 //start the loop
     while(playerHealth > 0) {
         turns +=1;
