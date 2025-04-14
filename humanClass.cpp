7715a0bd438d5e5a7d8b27f45419dc30c5b47faf
Pennywite13 <skmansard@gmail.com>
2025-04-14 11:23:52 -0500

    Created Classes Homework File

---
+++ humanClass.cpp
@@ -0,0 +1,34 @@
+//Classes in C++ Homework
+//April 14, 2025
+//Game Tech and Coding
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human{
+
+
+};
+
+int main(){
+    cout << "This is the begining of the code. \n";
+}
+
+
+
+
+/*
+INCLUDE FOR ASSIGNMENT
+
+string Name
+int Health = 10
+int Damage = 4
+a Constructor with parameters for name, health, baseDamage
+void SayHello()
+void SetHealth(int byAmount)
+void SetDamage(int givenDamage)
+int GetHealth()
+int GetDamage()
+
+*/
