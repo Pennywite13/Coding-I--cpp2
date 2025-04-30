757e90c70ba9295faf62389099cabba088e6b9d9
Pennywite13 <skmansard@gmail.com>
2025-04-30 17:19:39 -0500

    Video 1 complete
    finished video one for week 14, saved because its a good checkpoint and to eat dinner

--- headersAndFiles.cpp
+++ headersAndFiles.cpp
@@ -2,10 +2,54 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>         //file stream. to read and write to files
 using namespace std;
+
+void writeToFile(string givenString){
+    //open (or create) the file
+    ofstream file("file.txt", ios::app);    //ios::app part will update instead of overwrite
+
+    //confirm the file is open (early return if not open)
+    if(!file.is_open()){
+        cout << "Could not open file.\n";
+        return;
+    }
+    //write text to open file
+    file << givenString;
+
+    //close file (shich saves it to disk)
+    file.close();
+
+}
+
+void readFromFile(){
+    string fileContents;     //where we store the text in the file
+
+
+    //open the file
+    ifstream file("file.txt");
+
+    //check that its open
+    if(!file.is_open()){
+        cout << "Could not open file.\n";
+    }
+
+    //cout everything in file
+    while(getline(file, fileContents)){
+        cout << fileContents << "\n";
+    }
+
+    //close the file
+    file.close();
+
+}
 
 int main(){
     cout << "Lets write to some files! \n";
+
+    readFromFile();
+
+    writeToFile("Sylveon is the best pokemon!\n");
 
     return 0;
 }
