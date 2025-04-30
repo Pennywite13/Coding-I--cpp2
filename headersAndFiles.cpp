4599adf51c81f8a8f212c45c9d4e5025f2bdabd9
Pennywite13 <skmansard@gmail.com>
2025-04-30 16:45:32 -0500

    Setting up week 14 in class final

---
+++ headersAndFiles.cpp
@@ -0,0 +1,11 @@
+//week 14 coding I, reading and writing to files
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "Lets write to some files! \n";
+
+    return 0;
+}
